ab1d66da1b1e1e0788b6113dc716602dcc1a7762
Saad Khaleeq <saadkhaleeq610@gmail.com>
2024-07-12 10:54:19 -0700

    file updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(logger); // Move this line here
+app.use(logger);
 app.use('/api/posts', posts);
 
 app.listen(port, () => {
